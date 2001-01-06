4ef2e87622c7030f1b5d26561ac0dc70e9638557
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2001-01-06 00:48:32 +0000

    Now adds watch lists.

--- ConvertUsers.pl
+++ ConvertUsers.pl
@@ -7,7 +7,8 @@
 use DBI;
 use strict;
 
-my $user_id_seq = "users_id_seq";
+my $user_id_seq    = "users_id_seq";
+my $watch_list_seq = "watch_list_id_seq";
 
 &main();
 
@@ -39,7 +40,7 @@ sub main {
    $sql = "select id, username, password, cookie, firstlogin, lastlogin, email, watchnotifyfrequency, \
                   emailsitenotices_yn, emailbouncecount, type
              from users
-            order by id";
+            order by id limit 1";
 
    print "sql is $sql\n";
 
@@ -54,7 +55,10 @@ sub main {
 
 
       print $row->{"id"} . " " . $row->{"username"} . "\n";
-      UpdateUser($row, $dbh_pg);
+      my $user_id = UpdateUser($row, $dbh_pg);
+
+      TransferWatchLists($id, $user_id, $dbh_mysql, $dbh_pg);
+
       $num_users++;
    }
 
@@ -76,7 +80,6 @@ sub UpdateUser($;$) {
 
    my $sth;      
    my $sql;
-   my @row;
 
    my $name                 = $dbh->quote($row->{username});
    my $password             = $dbh->quote($row->{password});
@@ -119,6 +122,243 @@ sub UpdateUser($;$) {
 
    return $user_id;
 }
+
+sub TransferWatchLists($;$;$;$) {
+   my $user_id_mysql = shift;
+   my $user_id_pg    = shift;
+   my $dbh_mysql     = shift;
+   my $dbh_pg        = shift;
+
+   my $sth;
+   my $sql;
+   my $row;    
+   $sql = "select id, name from watch where owner_user_id = $user_id_mysql";
+   print "TransferWatchLists sql = $sql\n";
+   
+   $sth = $dbh_mysql->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+
+   my $num_watch_lists = 0;
+   while ($row = $sth->fetchrow_hashref()) {
+      my $watch_list_id_mysql = $row->{"id"};
+      my $name                = $row->{"name"};
+
+      print "watch list " . $row->{"id"} . " " . $row->{"name"} . "\n";
+
+      my $watch_list_id_pg = GetWatchListId($user_id_pg, $name, $dbh_pg);
+      if (!defined($watch_list_id_pg)) {
+         $watch_list_id_pg = CreateWatchList($user_id_pg, $name, $dbh_pg);
+      }
+
+      TransferWatchListItems($watch_list_id_mysql, $watch_list_id_pg, $dbh_mysql, $dbh_pg);
+      $num_watch_lists++;
+   }
+
+   $sth->finish();
+}
+
+sub GetWatchListId($;$;$) {
+   my $user_id_pg = shift;
+   my $name       = shift;
+   my $dbh_pg     = shift;
+
+   my $sth;
+   my $sql;
+   my @row;
+    
+   my $lowername = $dbh_pg->quote($name);
+   $sql = "select id from watch_list where user_id = $user_id_pg and lower(name) = $lowername";
+   print "GetWatchListId sql = $sql\n";
+      
+   $sth = $dbh_pg->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+   
+   @row = $sth->fetchrow_array();
+    
+   $sth->finish();
+   
+   return $row[0];
+}
+
+sub CreateWatchList($;$) {
+   my $user_id_pg = shift;
+   my $name       = shift;       
+   my $dbh_pg     = shift;
+   
+   my $sth;
+   my $sql;
+   my @row;
+
+   my $quotedname = $dbh_pg->quote($name);
+
+   my $id = GetNextValue($watch_list_seq, $dbh_pg);
+
+   $sql = "insert into watch_list (id, user_id, name) values ($id, $user_id_pg, $quotedname)";
+   print "GetWatchListId sql = $sql\n";
+      
+   $sth = $dbh_pg->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+   
+   $sth->finish();
+   
+   return $id;
+}
+
+sub TransferWatchListItems($;$;$) {
+   my $watch_list_id_mysql = shift;
+   my $watch_list_id_pg    = shift;
+   my $dbh_mysql           = shift;
+   my $dbh_pg              = shift;
+   my $sth;      
+   my $sql;
+   my $row;
+
+   $sql = "select port_id from watch_port where watch_id = $watch_list_id_mysql";
+   print "TransferWatchListItems sql = $sql\n";
+
+   $sth = $dbh_mysql->prepare($sql);
+
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+
+   my $num_watch_list_items = 0;
+   while ($row=$sth->fetchrow_hashref()) {   
+      my $port_id_mysql = $row->{"port_id"};
+
+      print "watch list item $port_id_mysql\n";
+
+      my $port_element_id = GetPortElementId($port_id_mysql, $dbh_mysql, $dbh_pg);
+
+      AddToWatchList($watch_list_id_pg, $port_element_id, $dbh_pg);
+
+      $num_watch_list_items++;
+   }
+
+   $sth->finish();
+}
+
+sub AddToWatchList($;$) {
+   my $watch_list_id_pg = shift;  
+   my $port_element_id  = shift;
+   my $dbh              = shift;
+   my $sth;
+   my $sql;
+   
+   $sql = "insert into watch_list_element (watch_list_id, element_id) values ($watch_list_id_pg, $port_element_id)";
+   print "AddToWatchList sql = $sql\n";
+    
+   $sth = $dbh->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+           
+   $sth->finish();
+}
+
+
+sub GetPortElementId($;$;$) {
+   my $port_id_mysql = shift;
+   my $dbh_mysql     = shift;
+   my $dbh_pg        = shift;
+
+   my $category_id = GetPortCategory($port_id_mysql, $dbh_mysql);
+
+   if (!defined($category_id)) {
+      die "GetPortElementId failed to find a category for port $port_id_mysql\n";
+   }
+
+   my $portname = GetPortName($port_id_mysql, $dbh_mysql);
+           
+   if (!defined($portname)) {
+      die "GetPortElementId failed to find a name for port $port_id_mysql\n";
+   }
+
+   my $category_name = GetCategoryName($category_id, $dbh_mysql);
+   if (!defined($category_name)) {
+      die "GetPortElementId failed to find a name for category $category_id\n";
+   }
+
+   my $port_element_id = GetIDFromPath("ports/$category_name/$portname", $dbh_pg);
+
+   if (!defined($port_element_id)) {
+      die "GetPortElementId failed to find an element id for ports/$category_name/$portname\n";
+   }
+
+   return $port_element_id;
+
+}
+
+sub GetPortCategory($;$) {
+   my $port_id = shift;
+   my $dbh     = shift;
+   my @row;
+
+   my $sql;
+   my $sth;
+
+   $sql = "select primary_category_id from ports where id = $port_id";
+
+   print "GetPortCategory sql = $sql\n";
+      
+   $sth = $dbh->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+
+   @row = $sth->fetchrow_array();
+
+   $sth->finish();
+
+   return $row[0];
+}
+
+sub GetCategoryName($;$) {
+   my $category_id = shift;
+   my $dbh     = shift;
+   my @row;
+
+   my $sql;
+   my $sth;
+
+   $sql = "select name from categories where id = $category_id";
+
+   print "GetCategoryName sql = $sql\n";
+
+   $sth = $dbh->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+    
+   @row = $sth->fetchrow_array();
+   
+   $sth->finish();
+   
+   return $row[0];
+}   
+
+sub GetPortName($;$) {
+   my $port_id = shift;
+   my $dbh     = shift;
+   my @row;
+      
+   my $sql;
+   my $sth;
+
+   $sql = "select name from ports where id = $port_id";
+
+   print "GetPortName sql = $sql\n";
+
+   $sth = $dbh->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+
+   @row = $sth->fetchrow_array();
+
+   $sth->finish();
+
+   return $row[0];
+}
+
 
 sub GetUser($;$) {
    my $username = shift;
@@ -180,5 +420,27 @@ sub GetNextValue($;$) {
                                     
    $sth->finish();
       
+   return $row[0];
+}
+
+sub GetIDFromPath($;$) {
+   my $pathname = shift;
+   my $dbh      = shift;
+   my $sth;
+   my $sql;
+   my @row;
+
+   $sql = "select Pathname_ID('$pathname')";
+      
+   print "sql is $sql\n";
+
+   $sth = $dbh->prepare($sql);
+   $sth->execute ||
+           die "Could not execute SQL $sql ... maybe invalid?";
+    
+   @row = $sth->fetchrow_array();
+   
+   $sth->finish();
+   
    return $row[0];
 }
